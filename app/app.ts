2dba3dfcd4be96a05630494626950aace7bc83df
unknown <gardikoury@gmail.com>
2018-10-22 12:25:38 -0200

    first commit

---
+++ app/app.ts
@@ -0,0 +1,13 @@
+// lib/app.ts
+import express = require('express');
+
+// Create a new express application instance
+const app: express.Application = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
